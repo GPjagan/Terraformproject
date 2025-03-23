df1ffeffee10c898c9a61bd64ca77689ee353829
gpjagan17 <gpjagan17@gmail.com>
2025-03-23 23:48:02 +0530

    Development workflow deploy next step - 7

--- Terraformproject/main.tf
+++ Terraformproject/main.tf
@@ -1,26 +1,51 @@
- # GCP Provider
+# # GCP Provider
+# provider "google" {
+#   project     = "upheld-castle-452119-c2"
+#   region      = "us-central1"
+#   credentials = file("/workspace/terraform-deployer.json")
+# }
+#
+# # VM Instance
+# resource "google_compute_instance" "my_instance" {
+#   name         = "cloudbuild-vm"
+#   machine_type = "e2-medium"
+#   zone         = "us-central1-a"
+#
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
+#
+#   network_interface {
+#     network = "default"
+#     access_config {}
+#   }
+#
+#   tags = ["cloudbuild", "ci-cd"]
+# }
+
  provider "google" {
-   project     = "upheld-castle-452119-c2"
-   region      = "us-central1"
-   credentials = file("/workspace/terraform-deployer.json")
- }
+  credentials = file("/workspace/terraform-deployer.json")
+  project     = var.project_id
+  region      = var.region
+}
 
- # VM Instance
- resource "google_compute_instance" "my_instance" {
-   name         = "cloudbuild-vm"
-   machine_type = "e2-medium"
-   zone         = "us-central1-a"
+resource "google_storage_bucket" "IAM-STORAGE-BUCKET" {
+  name     = "example-bucket-${random_id.bucket_id.hex}"
+  location = var.region
 
-   boot_disk {
-     initialize_params {
-       image = "debian-cloud/debian-11"
-     }
-   }
+  uniform_bucket_level_access = true
+}
 
-   network_interface {
-     network = "default"
-     access_config {}
-   }
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
 
-   tags = ["cloudbuild", "ci-cd"]
- }
+terraform {
+  backend "gcs" {
+    bucket = "IAM-STORAGE-BUCKET"
+    prefix = "terraform/state"
+  }
+}
+
---
+++ Terraformproject/variable.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+  default     = "upheld-castle-452119-c2"
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
