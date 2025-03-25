f70f2f39c3a4aa1d7e44c3a54696ebe847077009
gpjagan17 <gpjagan17@gmail.com>
2025-03-25 12:08:42 +0530

    Development workflow deploy next step - 30

--- main.tf
+++ main.tf
@@ -80,9 +80,40 @@ resource "google_storage_bucket" "log_bucket" {
   }
 }
 
+resource "google_compute_instance" "vm_ware" {
+  name         = "my-test-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
 # IAM Permissions for Cloud Build Service Account
 resource "google_storage_bucket_iam_member" "cloudbuild_writer" {
   bucket = google_storage_bucket.log_bucket.name
   role   = "roles/storage.objectAdmin"
   member = "serviceAccount:${var.cloudbuild_sa}"
 }
+
+
